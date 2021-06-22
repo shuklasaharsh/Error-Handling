3f54c6365a4ca3c0049ebedcce1d20bb88a16d83
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-06-22 15:53:01 +0530

    v0.1-Backend

--- playground.js
+++ playground.js
@@ -1,10 +1,9 @@
-const mailUtilities = require('./mailUtilities')
 const errorHandler = require('./errorHandler')
-let sendTo = "shukla.saharsh7@gmail.com"
-
 
 try {
     console.log(asjdal)
 } catch (e) {
     errorHandler.errorHandling(e)
 }
+
+
